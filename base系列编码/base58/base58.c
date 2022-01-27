237bac011158c0d04874f542fe74ce398555c3c8
Lu1u <78470445+smallboyZ@users.noreply.github.com>
2022-01-27 16:13:54 +0800

    Update base58.c

--- base系列编码/base58/base58.c
+++ base系列编码/base58/base58.c
@@ -15,20 +15,18 @@ int  find(char* table, char k) {
 }
 
 void b58encode(char* src) {
-	int  i = 0,j,tmp;
-	int len = 138 * strlen(src) / 100 + 1;                // 8 / log2(58)
+	int  i = 0, j, tmp;
+	int len = 138 * strlen(src) / 100 + 1;								 // 8 / log2(58)
 
 	uint8_t* enc = (uint8_t*)malloc(len * sizeof(uint8_t));   //以unsigned int8 来运算 char的范围 -128~127
 	memset(enc, 0, len * sizeof(uint8_t));
 
-	while (i<strlen(src) ){
+	while (i < strlen(src)) {
 		tmp = src[i];
-		for (j = len - 1; ; j--) {         //大端序 从低位修改进位改变enc的状态，做到大数进制转换。
+		for (j = len - 1;j>=0 ; j--) {													//大端序 从低位修改进位改变enc的状态，做到大数进制转换。
 			tmp += 256 * enc[j];
 			enc[j] = tmp % 58;
 			tmp = tmp / 58;
-			if (tmp == 0)
-				break;
 		}
 		i += 1;
 	}
@@ -37,14 +35,14 @@ void b58encode(char* src) {
 	while (!enc[i])
 		i++;
 	for (; i < len; i++) {
-		printf("%c",b58table[enc[i]]);
+		printf("%c", b58table[enc[i]]);
 	}
 	printf("\n");
 }
 
 void b58decode(char* enc) {
 	int  i = 0, j, tmp;
-	int len = 100 * strlen(enc) / 138 + 1;           
+	int len = 100 * strlen(enc) / 138 + 1;
 
 	for (int i = 0; i < strlen(enc); i++)
 		enc[i] = find(b58table, enc[i]);
@@ -53,24 +51,23 @@ void b58decode(char* enc) {
 	memset(m, 0, len * sizeof(uint8_t));
 
 	i = 0;
-	while (i<strlen(enc)){
+	while (i < strlen(enc)) {
 		int tmp = enc[i];
-		for (j = len - 1; ; j-=1) {
+		for (j = len - 1;; j -= 1) {
 			tmp += 58 * m[j];
 			m[j] = tmp % 256;
 			tmp = tmp / 256;
-			if (tmp == 0)
-				break;
+			if (!j) break;
 		}
 		i += 1;
 
-/*		int index = 0;                          检测运算状态的代码
-		while (!m[index])
-			index+=1;
-		for (; index < len; index++) {
-			printf("%u  ", m[index]);
-		}
-		printf("\n");*/
+		/*		int index = 0;                          检测运算状态的代码
+				while (!m[index])
+					index+=1;
+				for (; index < len; index++) {
+					printf("%u  ", m[index]);
+				}
+				printf("\n");*/
 	}
 
 	i = 0;
@@ -83,8 +80,8 @@ void b58decode(char* enc) {
 }
 
 int main() {
-	char m[] = "flag";  //flag{welcom_to_play_with_Lu1u}
-	char enc[] = "3cr9Ae";
+	char m[] = "flag{welcom_to_play_with_Lu1u}";  
+	char enc[] = "MXKxtLXrV7YyxXLQhKsLPC7o8HXv2ouwJuaXbabQx";
 	b58encode(m);
 	b58decode(enc);
 	return 0;
